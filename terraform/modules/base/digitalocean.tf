257c76670d1bf10989fe5f2b14ca4d3c0723c896
sal <ximena.salgadouribe@epfl.ch>
2023-01-29 12:07:16 +0100

    initial commit

---
+++ terraform/modules/base/digitalocean.tf
@@ -0,0 +1,28 @@
+resource "digitalocean_ssh_key" "ssh_key" {
+  name       = var.ssh_key_name
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+locals {
+  cloud_init_yml = templatefile("${path.module}/files/cloud-init.yml", {
+    user                   = var.droplet_user
+    init_ssh_public_key    = tls_private_key.ssh_key.public_key_openssh
+    docker_compose_version = var.docker_compose_version
+  })
+}
+
+resource "digitalocean_domain" "domain" {
+  name = var.domain_name
+}
+
+resource "digitalocean_project" "do_project" {
+  name        = var.do_project_name
+  description = var.do_project_description
+}
+
+resource "digitalocean_project_resources" "resources" {
+  project = digitalocean_project.do_project.id
+  resources = [
+    digitalocean_domain.domain.urn
+  ]
+}
---
+++ unite_compress/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from unite_compress.users.models import User
+from unite_compress.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
